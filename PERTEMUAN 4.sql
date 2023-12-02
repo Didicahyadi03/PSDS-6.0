ad6d2eda2f427d08497d085c73be879a30ce99b0
Didicahyadi03 <115863514+Didicahyadi03@users.noreply.github.com>
2023-12-03 00:05:25 +0700

    Add files via upload

---
+++ PERTEMUAN 4.sql
@@ -0,0 +1,27 @@
+SELECT * FROM Invoice i 
+
+SELECT CustomerId ,FirstName, City,State FROM Customer c 
+
+SELECT * FROM Invoice i WHERE Billingcity = 'Boston'
+
+SELECT * FROM Artist
+
+SELECT CustomerId, Billingcity, BillingState, BillingCountry from Invoice
+
+SELECT Lastname, Firstname, Birthdate FROM Employee
+
+SELECT * FROM Invoice 
+
+SELECT CustomerId, BillingAddress, Total FROM Invoice
+
+SELECT * FROM Employee
+
+SELECT * FROM Artist WHERE Name = 'Marcos Valle'
+
+SELECT * FROM Customer a WHERE City = 'Prague' or 'Oslo'
+
+SELECT DISTINCT Country FROM Customer
+
+
+
+
